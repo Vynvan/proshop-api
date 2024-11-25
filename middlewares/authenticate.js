c23eff83090152f88deedb923d438f39b50adddd
Vynvan <68636193+Vynvan@users.noreply.github.com>
2024-11-25 01:20:16 +0100

    Added jsdoc commentary to the middleware

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -1,6 +1,19 @@
+/**
+ * @module Middleware
+ */
 import jwt from 'jsonwebtoken';
 import 'dotenv/config';
 
+/**
+ * Middleware that verifies the jwt in the header and throws 403 if this verification fails.
+ * 
+ * @function
+ * @name authenticate
+ * @param {string} req.header The request header.
+ * @param {string} req.header.authorization Required - Holds the bearer token. 
+ * @returns {Object} Adds req.user holding the user.id for usage in the authenticated routes.
+ * @throws {403} An error message in JSON format with errorType: token.
+ */
 const authenticate = async (req, res, next) => {
    const token = req.headers.authorization?.split(' ')[1];
    if (!token) return res.status(401).json({ message: 'Nicht autorisiert!', errorType: 'token' });
